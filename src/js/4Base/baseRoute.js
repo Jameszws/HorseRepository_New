f1e97e66b3a09c01bab254652653e88ade581914
wszhang <wszhang@ctrip.com>
2017-06-09 15:40:09 +0800

    del old file

--- src/js/4Base/baseRoute.js
+++
@@ -1,8 +0,0 @@
-/**
- * 建立baseRoute
- */
-define(['angular'], function (angular) {
-	//确定依赖关系
-    var baseRoute = angular.module('myapp.route', ["ui.router", 'myapp.service','myapp.controller']);    
-    return baseRoute;
-});
